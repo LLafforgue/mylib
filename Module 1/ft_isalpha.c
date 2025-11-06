9f0f76e459d82668cd88e5302a3be018a5a71a86
Ludovic Lafforgue <ludovic.lafforgue.c@gmail.com>
2025-11-06 17:57:45 +0100

    Replace ft_isalpha with ft_lstiter function
    Removed ft_isalpha function and added ft_lstiter function.

--- Module 1/ft_isalpha.c
+++ Module 1/ft_isalpha.c
@@ -1,29 +1,6 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_isalpha.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: llafforg <llafforg@learner.42.tech>        +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/03 14:23:28 by llafforg          #+#    #+#             */
-/*   Updated: 2025/11/03 14:23:31 by llafforg         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include "libft.h"
 
-int	ft_isalpha(char *str)
+void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
-	if (*str == 0)
-		return (1);
-	else
-	{
-		if ((*str >= 'A' && *str <= 'Z') || (*str >= 'a' && *str <= 'z'))
-		{
-			if (*(str + 1))
-				return (ft_isalpha(str + 1));
-			else
-				return (1);
-		}
-		else
-			return (0);
-	}
+
 }
