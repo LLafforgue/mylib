a17ad425bc191a1b99a01ca6fccb9c0875926a9d
Ludovic Lafforgue <llafforg@c1r10s2.marseille.42.school>
2025-11-18 19:15:03 +0100

    test

---
+++ Test/get_next_line.c
@@ -0,0 +1,107 @@
+#include "get_next_line.h"
+#include <stdio.h>
+#include <fcntl.h>
+
+
+int ft_strlen(char *s)
+{
+	char	*temp;
+
+	temp = s;
+	while (*s)
+		s++;
+	return (s - temp);
+}
+
+char	*ft_join(char *buff, char *str, int i, int len)
+{
+	char	*temp;
+	char	*join;
+	int		init;
+
+	init = i;
+	while (i < len && buff[i] != '\n')
+		i++;
+	join = malloc(sizeof(char) * (i - init + ft_strlen(str) + 1));
+	if (!join)
+		return (NULL);
+	temp = join;
+	while (*str)
+		*join++ = *str++;
+	while (init < len && buff[init] != '\n')
+	{
+		*join++ = buff[init++];
+		if (buff[init] == '\n')
+			*join++ = buff[init];
+	}
+	*join = '\0';
+	return (temp);
+}
+
+int	ft_chek_line(char *str)
+{
+	while (*str)
+	{
+		if (*str++ == '\n')
+			return (1);
+	}
+	return (0);
+}
+
+char *get_next_line(int fd)
+{
+	static char	buff[BUFF_SIZE];
+	static int	byte_read;
+	static int	i;
+	char		*str;
+	char		*temp;
+
+	str = malloc(sizeof(char));
+	if (!str)
+		return (NULL);
+	*str = '\0';
+	if(i && byte_read)
+	{
+		temp = str;
+		str = ft_join(buff, str, i, byte_read);
+		free (temp);
+		if (buff[i] == '\n')
+			i++;
+	}
+	while (!ft_chek_line(str))
+	{
+		i = 0;
+		byte_read = read(fd, buff, BUFF_SIZE);
+		temp = str;
+		str = ft_join(buff, str, i, byte_read);
+		free(temp);
+		if (byte_read == -1)
+		{
+			free(str);
+			return (NULL);
+		}
+		if (!byte_read)
+			break;
+	}
+	while (buff[i] != '\n' && i < byte_read)
+		i++;
+	return (str);
+} 
+
+int main()
+{
+	int		fd;
+	int		cnt = 5;
+	int		deb = 0;
+	char	*line;
+
+	fd = open("./test", O_RDONLY);
+	while (deb < cnt)
+	{
+		line = get_next_line(fd);
+		// printf("main : %s-----\n", line);
+		deb++;
+	}
+	close(fd);
+	free(line);
+} 
---
+++ Test/get_next_line.h
@@ -0,0 +1,14 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <unistd.h>
+#include <stdlib.h>
+
+#ifndef BUFF_SIZE
+# define BUFF_SIZE 5
+
+#endif
+
+// char *get_next_line(int fd);
+
+#endif
